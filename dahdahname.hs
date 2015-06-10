74b7d88c22ea1a2a475acfb14f51d94f83e4152b
Thomas Levine <_@thomaslevine.com>
2015-06-10 18:52:39 +0000

    j

--- dahdahname.hs
+++ dahdahname.hs
@@ -3,42 +3,24 @@ import Data.List (intersperse)
 
 main :: IO ()
 main = do
-  argv <- getArgs
-  putStrLn $ parse argv
+  putStrLn $ unwords $ split '/' "aoeu/oeu/oeu/u"
+--argv <- getArgs
+--putStrLn $ concat $ intersperse "/" $ map parse argv
 
-parse :: [String] -> String
-parse argv = concat $ intersperse "/" argv
+split :: String -> String -> [String]
+split delimeter "" = []
+split delimeter original = left:(split delimeter $ tail right)
+  where
+    (left, right) = break (== delimeter) "oeu/oeu/oeu"
+
+parse :: String -> String
+parse x = x
+
+
+
+
 
 {-
-def parse(index):
-    for theclass in [basestring, datetime.date, datetime.datetime, int, type(None)]:
-        if isinstance(index, theclass):
-            yield from parse_partial(index) # tuple
-            break
-    else:
-        yield from itertools.chain(*map(parse, index)) # iterable
-
-_special = {'.': '\\.', '..': '\\..', '.tmp': '\\.tmp'}
-def replace_special(path):
-    for item in path:
-        if item in _special:
-            yield _special[item]
-        else:
-            yield item
-
-def parse_partial(item):
-    if isinstance(item, basestring):
-        func = parse_partial_text
-    elif isinstance(item, int):
-        func = lambda x: (str(x),)
-    elif isinstance(item, datetime.date) or isinstance(item, datetime.datetime):
-        func = parse_partial_date
-    elif item == None:
-        func = lambda _: ('',)
-    else:
-        raise ValueError('item must be string, datetime.date, datetime.datetime or int')
-    return tuple(func(item))
-#   yield from func(item)
 
 def parse_partial_text(item):
     for a in parse_partial_url(item):
